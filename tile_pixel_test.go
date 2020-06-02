601fee4d5f330e7d49afe74a47aea1fa0fc17b0d
Liu`e <liuwanle2010@gmail.com>
2020-06-02 10:26:21 +0800

    更新WGS84toZXY参数顺序

--- tile_pixel_test.go
+++ tile_pixel_test.go
@@ -5,4 +5,6 @@ import "testing"
 func Test_Num2WebMCBound(t *testing.T) {
 	t.Log(ZXYtoWebMCBound(12, 3429, 1673))
 	t.Log(ZXYtoWGS84Bound(12, 3429, 1673))
+
+	t.Log(WGS84toZXY(121.431863, 31.027647, 12))
 }
